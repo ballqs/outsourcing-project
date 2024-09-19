b5d43506e80a6c928340ad74d4ed4359281d05e4
ballqs <46590521+ballqs@users.noreply.github.com>
2024-09-19 14:02:07 +0900

    init

---
+++ src/main/java/org/sparta/outsourcingproject/OutsourcingProjectApplication.java
@@ -0,0 +1,13 @@
+package org.sparta.outsourcingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsourcingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutsourcingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sparta/outsourcingproject/OutsourcingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.outsourcingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsourcingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
