282756bc8ad2bf8805e9eb91cbe466d77fb090fb
ballqs <46590521+ballqs@users.noreply.github.com>
2024-09-20 21:17:47 +0900

    OrdersControllerTest 작성

--- src/test/java/org/sparta/outsourcingproject/common/data/TestData.java
+++ src/test/java/org/sparta/outsourcingproject/common/data/TestData.java
@@ -2,13 +2,14 @@ package org.sparta.outsourcingproject.common.data;
 
 import org.sparta.outsourcingproject.common.config.JwtUtil;
 import org.sparta.outsourcingproject.common.dto.AuthUser;
+import org.sparta.outsourcingproject.domain.user.Authority;
 
 public class TestData {
 
-//    public static final AuthUser AUTH_USER = new AuthUser(1L , "test@test.com" , Authorized.ADMIN);
-//    public static String TOKEN;
-//
-//    public static void initToken(JwtUtil jwtUtil) {
-//        TOKEN = jwtUtil.createToken(1L , "test@test.com" , Authorized.ADMIN);
-//    }
+    public static final AuthUser AUTH_USER = new AuthUser(1L , "test@test.com" , Authority.USER);
+    public static String TOKEN;
+
+    public static void initToken(JwtUtil jwtUtil) {
+        TOKEN = jwtUtil.createToken(1L , "test@test.com" , Authority.USER);
+    }
 }
--- src/test/java/org/sparta/outsourcingproject/domain/order/controller/OrderControllerTest.java
+++ src/test/java/org/sparta/outsourcingproject/domain/order/controller/OrderControllerTest.java
@@ -2,18 +2,36 @@ package org.sparta.outsourcingproject.domain.order.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.sparta.outsourcingproject.common.config.JwtFilter;
 import org.sparta.outsourcingproject.common.config.JwtUtil;
+import org.sparta.outsourcingproject.common.data.TestData;
 import org.sparta.outsourcingproject.common.dto.AuthUser;
+import org.sparta.outsourcingproject.domain.order.dto.OrderDetailSelectDto;
+import org.sparta.outsourcingproject.domain.order.dto.OrdersResponseSelectDto;
+import org.sparta.outsourcingproject.domain.order.dto.OrdersSelectDto;
+import org.sparta.outsourcingproject.domain.order.entity.OrderDetail;
+import org.sparta.outsourcingproject.domain.order.entity.Orders;
 import org.sparta.outsourcingproject.domain.order.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = {OrderController.class})
 class OrderControllerTest {
@@ -40,8 +58,79 @@ class OrderControllerTest {
         mvc = MockMvcBuilders.standaloneSetup(orderController)
                 .addFilters(new JwtFilter(jwtUtil)).build();
 
-//        this.authUser = new AuthUser(1L , "test@test.com");
-//        this.token = TestCommonData.TOKEN;
+        TestData.initToken(jwtUtil);
+        this.authUser = TestData.AUTH_USER;
+        this.token = TestData.TOKEN;
+    }
+
+    @Test
+    public void 주문_상태_변경_동작_완료() throws Exception {
+        // given
+        long orderId = 1L;
+
+        doNothing().when(ordersService).changeStatus(any(), anyLong());
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/api/orders/change-status/{orderId}" , orderId)
+                .header(HttpHeaders.AUTHORIZATION , token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isOk()).andDo(print());
+    }
+
+    @Test
+    public void 주문_단건_조회_동작_완료() throws Exception {
+        // given
+        long orderId = 1L;
+
+        Orders orders = new Orders();
+        OrderDetail orderDetail = new OrderDetail();
+        OrdersSelectDto ordersSelectDto = new OrdersSelectDto(orders);
+        OrderDetailSelectDto orderDetailSelectDto = new OrderDetailSelectDto(orderDetail);
+        List<OrderDetailSelectDto> orderDetailSelectDtos = new ArrayList<>();
+        orderDetailSelectDtos.add(orderDetailSelectDto);
+        OrdersResponseSelectDto ordersResponseSelectDto = new OrdersResponseSelectDto(ordersSelectDto , orderDetailSelectDtos);
+
+        given(ordersService.getOrder(anyLong())).willReturn(ordersResponseSelectDto);
+
+        // when
+        ResultActions resultActions = mvc.perform(get("/api/orders/{orderId}" , orderId)
+                .header(HttpHeaders.AUTHORIZATION , token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isOk()).andDo(print());
+    }
+
+    @Test
+    public void 주문_다건_조회_동작_완료() throws Exception {
+        // given
+        Orders orders = new Orders();
+        OrderDetail orderDetail = new OrderDetail();
+        OrdersSelectDto ordersSelectDto = new OrdersSelectDto(orders);
+        OrderDetailSelectDto orderDetailSelectDto = new OrderDetailSelectDto(orderDetail);
+        List<OrderDetailSelectDto> orderDetailSelectDtos = new ArrayList<>();
+        orderDetailSelectDtos.add(orderDetailSelectDto);
+        OrdersResponseSelectDto ordersResponseSelectDto = new OrdersResponseSelectDto(ordersSelectDto , orderDetailSelectDtos);
+        List<OrdersResponseSelectDto> ordersResponseSelectDtos = new ArrayList<>();
+        ordersResponseSelectDtos.add(ordersResponseSelectDto);
+
+        given(ordersService.getAllOrders(anyLong())).willReturn(ordersResponseSelectDtos);
+
+        // when
+        ResultActions resultActions = mvc.perform(get("/api/orders")
+                .header(HttpHeaders.AUTHORIZATION , token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isOk()).andDo(print());
     }
 
 }
