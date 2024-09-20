4679053710fef6926117457eb5b81573c3b35546
zerodeok <dudejr3165@naver.com>
2024-09-20 19:29:49 +0900

    add : authority추가 및 토큰값추가

--- src/main/java/org/sparta/outsourcingproject/common/dto/AuthUser.java
+++ src/main/java/org/sparta/outsourcingproject/common/dto/AuthUser.java
@@ -2,11 +2,13 @@ package org.sparta.outsourcingproject.common.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.sparta.outsourcingproject.domain.user.Authority;
 
 @Getter
 @AllArgsConstructor
 public class AuthUser {
     private Long userId;
     private String email;
+    private Authority authority;
 }
 
