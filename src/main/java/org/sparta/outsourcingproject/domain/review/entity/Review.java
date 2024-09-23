0d95485abf55aaa9095bd5efd725d9dc186636d7
kanz-in-code <parkhanjin1130@gmail.com>
2024-09-23 10:09:51 +0900

    리뷰 구현 (리뷰서비스 배달완료된건만 가능 부분 아직 미완(PostMapping 확인해야함))

--- src/main/java/org/sparta/outsourcingproject/domain/review/entity/Review.java
+++ src/main/java/org/sparta/outsourcingproject/domain/review/entity/Review.java
@@ -4,35 +4,35 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
+import lombok.*;
+import org.sparta.outsourcingproject.common.entity.Timestamped;
+
 
 @Entity
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor  // 기본 생성자 자동 생성
 @AllArgsConstructor // 모든 필드를 포함한 생성자 자동 생성
-public class Review {
+public class Review extends Timestamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long storeId;
     private Long orderId;
-    private String star;  // 1~5점
-    private String comment;
-    private LocalDateTime createdAt = LocalDateTime.now();  // 생성 시점 자동 기록
+    private int star;  // 1~5 별점
+    private String contents;
 
-    // 별도의 생성자가 필요한 경우
-    public Review(Long storeId, Long orderId, String star, String comment) {
+
+    public Review(Long storeId, Long orderId, int star, String contents) {
         this.storeId = storeId;
         this.orderId = orderId;
         this.star = star;
-        this.comment = comment;
-        this.createdAt = LocalDateTime.now();  // 생성 시간 자동 저장
+        this.contents = contents;
     }
+
+
+
 }
 
